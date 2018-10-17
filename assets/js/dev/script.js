9e458c28edfac311abed70dc82b3308a938fb596
Brent Robbins <brent@atendesigngroup.com>
2018-10-16 20:27:44 -0600

    nlcsrc-14: Added the mobile/reponsive nav (not completed) also started on the home facts responsive setup and footer.

--- assets/js/dev/script.js
+++ assets/js/dev/script.js
@@ -1,6 +1,18 @@
 function ready() {
 	// do your stuff
 	console.log('locked and loaded 🚀');
+
+	// Animate the mobile menu
+	document.querySelector('#nav-icon').addEventListener('click', function(e) {
+		document.querySelector('#nav-icon').classList.toggle('open');
+		document.querySelector('.navbar-menu').classList.toggle('open');
+		//document.querySelector('.navbar-burger').classList.toggle('open');
+
+		window.setTimeout(showLinks, 100);
+		function showLinks() {
+			document.querySelector('.nlcstar--site-nav').classList.toggle('open');
+		}
+	});
 }
 
 // this is required for the (not so) edge case where your script is loaded after the document has loaded
--- notes.md
+++ notes.md
@@ -15,6 +15,32 @@ https://github.com/dixonandmoe/rellax
 LAZY LOAD IMAGES?
 
 
+# To Do List:
+- [] Mobile/Responsive: header & menu
+- [x] Mobile/Responsive: footer
+- [] Mobile/Responsive: front page 'facts'
+
+
+
+
+.fill {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden
+}
+.fill img {
+    flex-shrink: 0;
+    min-width: 100%;
+    min-height: 100%
+}
+
+
+
+
+
+
+
 ### Global
 Site Width: 1238px ?
 
